775aa99f122427014e1d08febf40ab8812a6ec86
CafourekJ <jan.cafourek@infis.cz>
2025-04-01 08:31:21 +0200

    Add files via upload

---
+++ ukolA.py
@@ -0,0 +1,9 @@
+jidloJedna = str(input("Napište jídlo č. 1: "))
+KalorieJedna = int(input("Kolik mělo kalorií: "))
+jidloDva = str(input("Napište jídlo č. 2: "))
+KalorieDva = int(input("Kolik mělo kalorií: "))
+jidloTri = str(input("Napište jídlo č. 3: "))
+KalorieTri = int(input("Kolik mělo kalorií: "))
+
+celkove_kalorie = (KalorieJedna + KalorieDva + KalorieTri)
+print("Celkový počet kalorií je:",celkove_kalorie)
